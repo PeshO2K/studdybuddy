a231f2f361260875f5fd4496906f9e79ec9f16e8
PeshO2K <patienceotuke@gmail.com>
2024-09-18 01:40:00 +0300

    feat(log in): Added log in and sign in forms. No authentication currently.

---
+++ forms/generic_form.py
@@ -0,0 +1,62 @@
+import streamlit as st
+
+
+def generic_form(location='main', fields=None, key='Sample Form'):
+    
+    if location == 'main':
+        new_form = st.form(key=key,clear_on_submit=True)
+    elif location == 'sidebar':
+        new_form = st.sidebar.form(key=key, clear_on_submit=True)
+    elif location == 'hidden':
+        pass
+    else:
+        raise ValueError("Location must be main, sidebar or hidden")
+    
+    if fields is None:
+        fields = {
+            'Title': 'Sample Form',
+            'Subtitles': ['Subtitle 1', 'Subtitle 2'],
+            'Textfields':{
+            'Username' : 'Username',
+                            'Password': 'Password',
+                             'Password': "Confirm Password" },
+            'Button':'Submit'
+
+
+        }
+    if fields:
+        values = {}
+        for field_name, field_value in fields.items():
+            if field_name =='Title':
+                new_form.title(field_value,)
+            elif field_name == 'Subtitles':
+                for subtitle in field_value:
+                    new_form.subheader(subtitle)
+            elif field_name == 'Textfields':
+                for textfield in field_value:
+                    # new_form.text_input(textfield, type='password' if textfield ==
+                    #                     'Password' or textfield == 'Confirm Password' else 'default')
+                    values[textfield] = new_form.text_input(textfield, type='password' if textfield ==
+                                             'Password' or textfield == 'Confirm Password' else 'default')
+        
+        
+
+                
+        
+        # submit = new_form.form_submit_button(fields['Button'])
+        # new_form.link_button("SignUp Button",'Someurl')
+        # form_buttons = new_form.container()
+
+        # left, middle,right = form_buttons.columns(3,vertical_alignment="bottom")
+
+        submit = new_form.form_submit_button(
+            fields['Buttons']['Submit'], use_container_width=True)
+        # secondary_button = right.markdown(
+            # fields['Buttons']['Secondary'][0])
+        
+
+        values['submit'] = submit
+        
+    return values
+            
+            # st.text(f"{field_name}, {field_value}")
--- main_app.py
+++ main_app.py
@@ -1,24 +1,92 @@
 import streamlit as st
+from forms.login_form import login_form
+from forms.signup_form import signup_form
 
 
-#-------PAGE SETUP--------------
-about_page=st.Page(
-  page = "views/about.py",
-  title="About me",
-  icon="👀",
+# # #------Initialise session state attributes
+if 'authentication_status' not in st.session_state:
+    st.session_state.authentication_status = None
+
+if 'signing_in' not in st.session_state:
+   st.session_state.signing_in = False
+
+if 'nav_position' not in st.session_state:
+   st.session_state.nav_position = 'sidebar'
+
+# Function to handle log out
+
+
+def logout():
+  #  print("*******log out start ******")
+   st.session_state.authentication_status = None
+   st.session_state.signing_in = False
+   st.session_state.nav_position = 'hidden'
+   st.rerun()
+  #  print("*******log out end ******")
+
+# Update signin to true or false in order to switch pages from log in to signin when respective button is clicked
+
+
+def update_login_status():
+  st.session_state.signing_in = not st.session_state.signing_in
+  st.rerun()
+
+
+# Create Page objects for navigation
+about_page = st.Page(
+    page="views/about.py",
+    title="About me",
+    icon="👀",
 )
-project_1_page=st.Page(
-  page = "views/studybot.py",
-  title="Study Buddy",
-  icon="🧠",
-  default=True,
+project_1_page = st.Page(
+    page="views/studybot.py",
+    # page=btns,
+    title="Study Buddy",
+    icon="🧠",
+    # default=True,
 )
 
-pg=st.navigation(
-  {
-    "Projects":[project_1_page],
-    "Info":[about_page],
-  }
-)
-st.sidebar.text("Made By Pesh")
-pg.run()
+page_dict = {
+
+    "Projects": [project_1_page],
+    "Info": [about_page],
+
+}
+
+# ------ Login/SignUp pages or the Main app depending on authentication status
+if st.session_state.authentication_status:
+
+    pg = st.navigation(page_dict, position='sidebar'if st.session_state.authentication_status else 'hidden'
+                       )
+
+else:
+    if not st.session_state.signing_in:
+      signup_btn = st.sidebar.button("Sign Up", use_container_width=True)
+      pg = st.navigation([st.Page(login_form)])
+
+      st.title("Logging In Page")
+
+      if signup_btn:
+        update_login_status()
+
+    else:
+      login_btn = st.sidebar.button("Log in", use_container_width=True)
+
+      if login_btn:
+        update_login_status()
+      # signup_form()
+      pg = st.navigation([st.Page(signup_form)])
+
+      st.title("Signing In Page")
+
+pg.run()  # Render the selected page
+
+
+# Render a logout button
+if st.session_state.authentication_status:
+  with st.sidebar:
+    for idx in range(10):
+      st.empty().write(">>>>>>>>>>")
+  logout_btn = st.sidebar.button("Log out", use_container_width=True)
+  if logout_btn:
+      logout()
